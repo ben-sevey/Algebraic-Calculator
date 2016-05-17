3bbde73eb01a047d065f8bad430fdfa795ae3840
Ben Sevey <ben.sevey@gmail.com>
2016-05-16 21:08:06 -0400

    Validated variable terms and set rules for variable operations

--- equationParser.js
+++ equationParser.js
@@ -7,6 +7,18 @@ function trimSpace(str) {
 	return str;
 }
 
+function unitTest(str) {
+
+	var root = new Node();
+	root.leftChild = new Node();
+	root.rightChild = new Node();
+	root.value = '+';
+	root.leftChild.value = [5,0];
+	root.rightChild.value = [3,0];
+	
+	alert(interpretEquationTree(root));
+}
+
 /*
 	Build a tree for general arithmetic equations. Operators serve as inner nodes
 	and numerical terms serve as leaves. Tree is constructed with higher order
@@ -22,8 +34,12 @@ function constructEquationTree(str, currentNode){
 	
 	while (lowestOrderIndex === -1) {
 		// Base case registers when a valid numerical term is passed
+			// Variable terms in the form 4x+7 --> [7,4]
 		if (/^(?=.*\d)\d*(?:\.\d*)?$/.test(str)) {
-			return parseFloat(str);
+			return [parseFloat(str),0];
+		}
+		else if (/^(?:(?=.*\d)\d*(?:\.\d*)?)?[a-z]$/.test(str)) {
+			return [0,parseFloat(str.slice(0,-1))];
 		}
 		// Search the current equation for the lowest order operator
 			// Precedence follows standard mathematical order of operations
@@ -89,10 +105,13 @@ function constructEquationTree(str, currentNode){
 */
 function initiateEquationTree(str) {
 	
+	var output;
 	var root = new Node();
 	str = trimSpace(str);
 	root.value = constructEquationTree(str, root);
-	alert(interpretEquationTree(root));
+	
+	output = interpretEquationTree(root);
+	alert(output[1] + "x" + " + " + output[0]);
 }
 
 /*
@@ -108,21 +127,68 @@ function interpretEquationTree(root) {
 	}
 	
 	switch (root.value) {
-		case '-':
-			return interpretEquationTree(root.leftChild) - interpretEquationTree(root.rightChild);
-			break;
 		case '+':
-			return interpretEquationTree(root.leftChild) + interpretEquationTree(root.rightChild);
+			return add(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
 			break;
-		case '/':
-			return interpretEquationTree(root.leftChild) / interpretEquationTree(root.rightChild);
+		case '-':
+			return subtract(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
 			break;
 		case '*':
-			return interpretEquationTree(root.leftChild) * interpretEquationTree(root.rightChild);
+			return multiply(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
+			break;
+		case '/':
+			return divide(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
 			break;  
 		case '^':
 			return Math.pow(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
 			break;
+	}
+	
+	/*
+	/^(?=.*\d)\d*(?:\.\d*)?$/
+		Operation helper functions. Implement rules for variable operations.
+		
+		term parameters are arrays where the value stored at index i is equal
+		to the coefficient of x^i
+	*/
+	function add(term1, term2) {
+	
+		return [term1[0] + term2[0], term1[1] + term2[1]];
+	}
+	function subtract(term1, term2) {
+	
+		return [term1[0] - term2[0], term1[1] - term2[1]];
+	}
+	// Only capable of x^0 and x^1 order functions
+	function multiply(term1, term2) {
+	
+		// zero-power * zero-power
+		if (term1[1] === 0 && term2[1] === 0) {
+			return [term1[0] * term2[0],0];
+		}
+		// zero-power * (zero-power + first-power)
+		else if (term1[1] === 0) {
+			return [term1[0] * term2[0], term1[0] * term2[1]];
+		}
+		// (zero-power + first-power) * zero-power
+		else {
+			return [term1[0] * term2[0], term1[1] * term2[0]];
+		}
+	}
+	// Only capable of x^0 and x^1 order functions
+	function divide(term1, term2) {
+		// zero-power / zero-power
+		if (term1[1] === 0 && term2[1] === 0) {
+			return [term1[0] / term2[0],0];
+		}
+		// zero-power / (zero-power + first-power)
+		else if (term1[1] === 0) {
+			return [term1[0] / term2[0], term1[0] / term2[1]];
+		}
+		// (zero-power + first-power) / zero-power
+		else {
+			return [term1[0] / term2[0], term1[1] / term2[0]];
+		}
 	}
 }
 
