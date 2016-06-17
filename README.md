678aba299021f16e5307cfe18a302562b03d5aa2
Ben Sevey <ben.sevey@gmail.com>
2016-06-17 09:38:03 -0400

    Tightened up linear system solving to account for systems will no solutions and multiple solutions.

--- README.md
+++ README.md
@@ -3,13 +3,16 @@
 In the process of building a basic calculator web app. 
 The eventual goal is to be able to solve simple arithmetic as well as systems of linear equations.
 
-Current version allows solving of multi-variable linear systems, albeit with a few assumptions.
+Input an expression with any alphabetic characters (lowercase for now) to simplify.
+Ex. 8x * 2 + 9 * (y + y)  => 16x + 2y + 9
+Input two expressions separated by an equal sign to solve for the value of a single variable
+Ex. 5u - 7 = 2u + 11 => u = 6
+Input multiple linear equations, each separated by a comma to solve for the value of each variable
+Ex. x + y + z = 6, 3x - y - 2z = -3, -2x - 5y + 2z = 11 => x=2, y=-1, z=5 
 
 
 #TODO
 
 * Handle invalid input
-* Make output readable
-* Sort linear systems 
 * Build a shiny HTML page to interface with the user
 * Re-read comments
--- equationParser.js
+++ equationParser.js
@@ -1,8 +1,67 @@
-function unitTest(str) {
+// Global variables
+var opHelper = new expressionOperations();
+
+/*
+	Based on the input determine whether we need to simplify or solve,
+	and if solving is it a single- or multi-variable system.
+*/
+function inputHandler(input) {
+
+	var output;
+
+	if (input.includes('=')) {
+		if (input.includes(',')) {
+			output = multiVariableSolver(input.split(','));
+		}
+		else {
+			output = [singleVariableSolver(input)];
+		}
+		output = valueToString(output);
+	}
+	else {
+		output = expressionToString(simplifyExpression(input));
+	}
 	
-	var cut = str.split(',');
-	var output = multiVariableSolver(cut);
-	alert(JSON.stringify(output, null, 4));
+	if (output === "") {
+		output = "An Error occurred. Please check the input.";
+	}
+
+	alert(output);
+}
+
+/*
+	Given an expression object, convert into a string which
+	separates all terms by addition
+*/
+function expressionToString(expression) {
+	
+	var str = "";
+	for (var key in expression) {
+		str += expression[key].toFraction(false) + key + " + ";
+	}
+	
+	return str.slice(0, -3).replace(/_/g, "");
+}
+
+/*
+	Given an array of [variable, value] tuples, convert into a 
+	string assigning each variable to it's value.
+*/
+function valueToString(valueSet) {
+
+	var str = "";
+	for (var i=valueSet.length-1; i>=0; i--) {
+		if (valueSet[i][1] === '@') {
+			return "all solutions";
+		}
+		else if (valueSet[i][1] === '!') {
+			return "no solution";
+		}
+		else {
+			str += valueSet[i][0] + " = " + valueSet[i][1] + ",";
+		}
+	}
+	return str.slice(0,-1);
 }
 
 /*
@@ -32,20 +91,7 @@ function normalizeString(str) {
 	}
 	
 	str = str.replace(/\+-/g,'-'); // Addition of a negative is just subtraction
-	
 	return str;
-}
-
-function unitTest(str) {
-
-	var root = new Node();
-	root.leftChild = new Node();
-	root.rightChild = new Node();
-	root.value = '+';
-	root.leftChild.value = [5,0];
-	root.rightChild.value = [3,0];
-	
-	alert(interpretEquationTree(root));
 }
 
 /*
@@ -55,7 +101,6 @@ function unitTest(str) {
 	
 	Utilize Fraction.js to increase precision
 */
-<<<<<<< HEAD
 function expressionOperations() {
 	
 	/*
@@ -79,41 +124,6 @@ function expressionOperations() {
 		for (var key1 in expression1) {
 			if (expression2[key1]) {
 				result[key1] = Fraction(expression1[key1]).add(expression2[key1]);
-=======
-function constructEquationTree(str, currentNode){
-	
-	var inParen = false;
-	var parenCount = 0;
-	
-	var opOrder = 2;
-	var lowestOrderIndex = -1;
-	
-	while (lowestOrderIndex === -1) {
-		// Base case registers when a valid numerical term is passed
-			// Variable terms in the form 4x+7 --> [7,4]
-		if (/^(?=.*\d)\d*(?:\.\d*)?$/.test(str)) {
-			return [parseFloat(str),0];
-		}
-		else if (/^(?:(?=.*\d)\d*(?:\.\d*)?)?[a-z]$/.test(str)) {
-			return [0,parseFloat(str.slice(0,-1))];
-		}
-		// Search the current equation for the lowest order operator
-			// Precedence follows standard mathematical order of operations
-		for (var i=str.length-1;i>=0;i--) {
-			if (!inParen && opOrder > 0) {
-				if ((str[i] === '-' || str[i] === '+') && opOrder > 0){
-					opOrder = 0;
-					lowestOrderIndex = i;
-				}
-				else if ((str[i] === '*' || str[i] === '/') && opOrder > 1) {
-					opOrder = 1;
-					lowestOrderIndex = i;
-				}
-				else if (str[i] === '^') {
-					opOrder = 2;
-					lowestOrderIndex = i;
-				}
->>>>>>> origin/master
 			}
 			else {
 				result[key1] = Fraction(expression1[key1]).add(0);
@@ -211,45 +221,52 @@ function constructEquationTree(str, currentNode){
 	}
 	/*
 		Allow contant values to be raised to constant powers.
-		Need to adjust functionality to match Fraction.js
 	*/
 	this.raise = function raise(expression1, expression2) {
 		
 		var result = {};
-		result['_'] = Math.pow(expression1['_'], expression2['_']);
-			
+		var variableCheck = [];
+		
+		variableCheck = Object.keys(expression1);
+		variableCheck.concat(Object.keys(expression2));
+		
+		for (var i=0; i<variableCheck.length; i++) {
+			if (variableCheck[i] !== '_') {
+				// Only handles linear equations
+				alert("Don't Panic.");
+			}
+		}
+		
+		result['_'] = expression1['_'].pow(expression2['_']);
 		return result;
 	}
 }
 
 /*
-	Substitute known values for a given variable and return the 
-	resulting expression. Handles substitution into multi-variable
-	expression through multiple function calls.
+	HACKED TO PLAY NICELY WITH MULTIVARIABLESOLVER
+	RETURNS THE SUBSTITUTED VARIABLE WITH A COEFFICIENT
+	OF ZERO INSTEAD OF REMOVING THAT TERM
+
+	Substitute known values for a given variable into a simplified 
+	expression and return the resulting expression. Handles 
+	substitution into multi-variable expression through multiple 
+	function calls.
 	
 	expression 4x + 3 = {'x':4, '_':3}
-	value (x = 2) = ['x',2] 
+	value (x = 2) = ['x',{'_':2}] 
+	or value (x = (t - 3)) = ['x', {'t':1, '_':-3}]
 */
 function substitute(expression, value) {
 	
-	var result = {};
-	var opHelper = new expressionOperations();
-	
-<<<<<<< HEAD
-	result['_'] = Fraction(expression[value[0]]).mul(value[1]);	// Multiply coefficient by the known value
-	delete expression[value[0]];								// Remove given variable from the expression
-	result = opHelper.add(result, expression); 	
-	
-	return result;
-=======
-	var output;
-	var root = new Node();
-	str = trimSpace(str);
-	root.value = constructEquationTree(str, root);
-	
-	output = interpretEquationTree(root);
-	alert(output[1] + "x" + " + " + output[0]);
->>>>>>> origin/master
+		var result = {};
+		
+		result['_'] =  new Fraction(expression[value[0]]);
+		result = opHelper.multiply(result, value[1]);
+		
+		expression[value[0]] = new Fraction(0);
+		result = opHelper.add(result, expression);
+		
+		return result;
 }
 
 /*
@@ -265,7 +282,6 @@ function simplifyExpression(str) {
 	
 	return initiateEquationTree(str);
 	
-<<<<<<< HEAD
 	/*
 		Build a tree for general arithmetic equations. Operators serve as inner nodes
 		and numerical terms serve as leaves. Tree is constructed with higher order
@@ -381,27 +397,16 @@ function simplifyExpression(str) {
 		root.value = constructEquationTree(str, root);
 		
 		return interpretEquationTree(root);
-		/*
-		var terms = Object.keys(output);
-		for (var i=0; i<terms.length; i++) {
-			terms[i] = "" + output[terms[i]] + terms[i];
-		}
-		var simpExp = terms.join('+');
-		
-		alert(simpExp);
-		*/
 	}
 
 	/*
 		Given the root of a mathematical parse tree containing numerical
 		and operational (+,-,*,/,^) values, compute the result.
-		Need to update rules to handle operations with variables (x,y)
+		All direct operations use Fraction Objects to maintain precision
 	*/
 	function interpretEquationTree(root) {
-		
-		var opHelper = new expressionOperations();
-		
-		// Leaves should contain only terms that can be evaluated to a number
+				
+		// Leaves hold simplified terms that can be operated upon
 		if (!root.leftChild && !root.rightChild) {
 			return root.value;
 		}
@@ -433,72 +438,29 @@ function simplifyExpression(str) {
 		this.value;
 		this.leftChild;
 		this.rightChild;
-=======
-	switch (root.value) {
-		case '+':
-			return add(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
-			break;
-		case '-':
-			return subtract(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
-			break;
-		case '*':
-			return multiply(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
-			break;
-		case '/':
-			return divide(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
-			break;  
-		case '^':
-			return Math.pow(interpretEquationTree(root.leftChild),interpretEquationTree(root.rightChild));
-			break;
->>>>>>> origin/master
 	}
+}
+
+/*
+	Evalauate an expression in terms of the given variable.
 	
-	/*
-	/^(?=.*\d)\d*(?:\.\d*)?$/
-		Operation helper functions. Implement rules for variable operations.
-		
-		term parameters are arrays where the value stored at index i is equal
-		to the coefficient of x^i
-	*/
-	function add(term1, term2) {
+	Ex. Given ("y - z = 2", 'y') return [y, 'z + 2']
+*/
+function evaluateVariable(str, variable) {
 	
-		return [term1[0] + term2[0], term1[1] + term2[1]];
-	}
-	function subtract(term1, term2) {
+	var coeff = {};
+	var temp = str.split('=');
+	//var opHelper = new expressionOperations();
 	
-		return [term1[0] - term2[0], term1[1] - term2[1]];
-	}
-	// Only capable of x^0 and x^1 order functions
-	function multiply(term1, term2) {
+	// Send all terms to one side to achieve the form Ax +By + ... + C = 0
+	temp = opHelper.subtract(simplifyExpression(temp[0]), simplifyExpression(temp[1]));
+	coeff['_'] = temp[variable];
+	delete temp[variable];
 	
-		// zero-power * zero-power
-		if (term1[1] === 0 && term2[1] === 0) {
-			return [term1[0] * term2[0],0];
-		}
-		// zero-power * (zero-power + first-power)
-		else if (term1[1] === 0) {
-			return [term1[0] * term2[0], term1[0] * term2[1]];
-		}
-		// (zero-power + first-power) * zero-power
-		else {
-			return [term1[0] * term2[0], term1[1] * term2[0]];
-		}
-	}
-	// Only capable of x^0 and x^1 order functions
-	function divide(term1, term2) {
-		// zero-power / zero-power
-		if (term1[1] === 0 && term2[1] === 0) {
-			return [term1[0] / term2[0],0];
-		}
-		// zero-power / (zero-power + first-power)
-		else if (term1[1] === 0) {
-			return [term1[0] / term2[0], term1[0] / term2[1]];
-		}
-		// (zero-power + first-power) / zero-power
-		else {
-			return [term1[0] / term2[0], term1[1] / term2[0]];
-		}
-	}
+	temp = opHelper.multiply(opHelper.divide(temp, coeff), {'_':-1});
+	temp = expressionToString(temp);
+	
+	return [variable, temp];
 }
 
 /*
@@ -513,21 +475,26 @@ function singleVariableSolver(str) {
 	var keys;
 	var token;
 	var temp = str.split('=');
-	var opHelper = new expressionOperations();
 	
+	// Send all terms to one side to achieve the form Ax +By + ... + C = 0
 	temp = opHelper.subtract(simplifyExpression(temp[0]), simplifyExpression(temp[1]));
+	token = str.match(/[a-z]/)[0];
 	
-	keys = Object.keys(temp);
-	for (var i=0; i<keys.length; i++) {
-		if (/[a-z]/.test(keys[i])) {
-			token = keys[i];
-		}
+	if (temp[token].n !== 0) {
+		return [token, Fraction(-1).mul((Fraction(temp['_']).div(temp[token]))).toFraction()];
 	}
-	
-	return [token, Fraction(-1).mul((Fraction(temp['_']).div(temp[token])))];
+	else {
+		if (temp['_'].n === 0) {
+			return [token, "@"];
+		}
+		else {
+			return [token, "!"];
+		}
+	}	
 }
 
 /*
+	FIRST EQUATION MUST BE FILLED
 	Accept a series of equations with related
 	variables and successively solve for the 
 	value of each. Return a tuple array of 
@@ -539,20 +506,20 @@ function singleVariableSolver(str) {
 */
 function multiVariableSolver(strArr) {
 	
-	//========local variables declared here==================//
 	var equations = [];
-	var opHelper = new expressionOperations();
 	var keys;
+	var orderedKeys;
 	var result = [];
-	//=======================================================//
 	
 	// Put all equations into the form Ax + By + Cz + D = 0
 	for (var i=0; i<strArr.length; i++) {
 		var temp = strArr[i].split('=');
 		equations.push(opHelper.subtract(simplifyExpression(temp[0]),simplifyExpression(temp[1])));
 	}
+	// Assume first equation is filled (ie. contains all variables)
 	keys = Object.keys(equations[0]);
 	
+	// Fill equations by assigning missing terms a coefficient of zero
 	for (var i=1; i<equations.length; i++) {
 		for (var j=0; j<keys.length; j++) {
 			if (!equations[i][keys[j]]) {
@@ -571,6 +538,7 @@ function multiVariableSolver(strArr) {
 	}
 	
 	// Elementary row operations to reduce matrix to upper triangular
+		// Solution attained by Elimination
 	for (var i=0; i<keys.length-2; i++) {
 		for (var j=i+1; j<equations.length; j++){
 			if (equations[j][keys[i]].n !== 0) {
@@ -581,28 +549,74 @@ function multiVariableSolver(strArr) {
 		}
 	}
 	
+	/*
+		Reduce non-square systems to a squared system, and verify
+		that a solution space still exists.
+	*/
+	if (equations.length > keys.length-1) {
+		for (var i=equations.length-1; i >= keys.length-1; i--) {
+			var coeff = {};
+			coeff['_'] = Fraction(equations[keys.length-2][keys[keys.length-2]]).div(equations[i][keys[keys.length-2]]);
+			equations[i] = opHelper.subtract(equations[keys.length-2],opHelper.multiply(coeff,equations[i]));
+		}
+	}
+	// For non-square systems check that the system is still valid
+		// and trim excess equations to make the system square
+	while (equations.length > keys.length-1) {
+		var test = equations.pop();
+		
+		var stringy = test[keys[keys.length-2]].toFraction();
+		stringy = "0=" + stringy + keys[keys.length-2] + "+" + test['_'];
+		
+		// When any equation in the system has no solution,
+			// the entire system has no solution.
+		if (singleVariableSolver(stringy)[1] === '!') {
+			return [['x', '!']];
+		}
+	}
+	
+	// Substitute known values to create single variable equations and solve
 	for (var i=equations.length-1; i>=0; i--) {
-		keys = Object.keys(equations[i]);
-		for (var j=0; j<keys.length; j++) {
-			if (equations[i][keys[j]].n !== 0 && keys[j] !== '_') {
-				var stringy = equations[i][keys[j]].toFraction();
-				stringy = "0=" + stringy + keys[j] + "+" + equations[i]['_'];
-				result.push(singleVariableSolver(stringy));
+		
+		var stringy = "";
+		// Change each equation into a string, ommiting zero-coefficient terms
+		stringy += "0=" + equations[i]['_'].toFraction();
+		for (var j=0; j<keys.length-1; j++) {
+			if (equations[i][keys[j]].n !== 0) {
+				stringy += "+" + equations[i][keys[j]].toFraction() + keys[j];	
 			}
 		}
+		// Special cases when an equation contains a free parameter
+			// need to accept "0=0+0x" and "0=x+y" 
+		if (!stringy.match(/[a-z]/)) {
+			stringy += "+0" + keys[i]; 
+			result.push(singleVariableSolver(stringy));
+		}
+		else if (stringy.match(/[a-z]/g).length > 1) {
+			result.push(evaluateVariable(stringy, keys[i]));
+		}
+		else {
+			result.push(singleVariableSolver(stringy));
+		}
+		
+		// Prepare the next equation for solving by substituting all known values
 		if (i>0) {
 			for (var j=0; j<result.length; j++) {
-				equations[i-1] = substitute(equations[i-1], result[j]);
+				
+				var subValue = {};
+				switch(result[j][1]) {
+					case '!':
+						return [[result[j][0], '!']];
+					case '@':
+						result[j][1] = result[j][0];
+						break;
+				}
+				
+				subValue = simplifyExpression(result[j][1]);
+				equations[i-1] = substitute(equations[i-1], [result[j][0],subValue]);
 			}
 		}
 	}
 	return result;
 }
-/*
-	Hold a simplified expression of the form
-	A + Bx + Cy
-var term = {	
-	_: A,
-	x: B,
-	y: C
-}*/
+
